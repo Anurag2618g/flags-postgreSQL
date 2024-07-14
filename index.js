843d7b4c96b8cd044f0b16424ae9b421600e2374
Anurag2618g <anurag2618g@gmail.com>
2024-07-14 12:48:29 +0530

    modifications

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+const dbPassword = process.env.DB_PASSWORD;
 
 const app = express();
 const port = 3000;
@@ -11,7 +15,7 @@ const db = new pg.Client({
   user: "postgres",
   host: "localhost",
   database: "world",
-  password: "chained_86",
+  password: dbPassword,
   port: 5432,
 });
 
